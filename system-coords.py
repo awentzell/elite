7dbd00dfa2549cc5833a16b8b0ba33f071e1b8e3
awentzell <awentzell@users.noreply.github.com>
2020-08-02 15:28:37 -0400

    Update system-coords.py

--- system-coords.py
+++ system-coords.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-import csv, json, re, requests, sys, urllib
+import csv, json, re, requests, sys
 
 edsm_url = 'https://www.edsm.net/api-v1/system'
 
@@ -13,16 +13,11 @@ map_data = { "markers": []}
 with open(sys.argv[1]) as csvfile:
 	reader = csv.reader(csvfile)
 	for row in reader:
-		# system_name = urllib.quote(re.split('[ A-Z]+$', row[1])[0])
 		system_name = re.split('[ A-Z]+$', row[1])[0]
-		# print '%s\t%s\t%s' % (row[0], row[1], system_name)
 		resp = requests.get(url=edsm_url, params=dict(systemName=system_name, showCoordinates=1))
 		data = resp.json()
 		if data == []:
 			data = { "coords": {'x': 0, 'y': 0, 'z': 0}}
-
-		# print json.dumps(data)
-		# print
 
 		map_data['markers'].append(
 			{
